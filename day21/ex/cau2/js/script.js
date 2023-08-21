5b5e6eacaefe6d38d89a867621e1d0b49f4cd590
Nguyen Duc <duc011002@gmail.com>
2023-08-22 06:52:04 +0700

    home

--- day21/ex/cau2/js/script.js
+++ day21/ex/cau2/js/script.js
@@ -19,10 +19,28 @@ Viết một hàm tạo (Constructor) để khởi tạo ra một đối tượn
 Sau đó viết một hàm nhận vào một mảng chứa nhiều đối tượng để khởi tạo ra một mảng mới chứa các đối tượng có cấu trúc như trên.
 
 Kết quả trả về là một mảng chứa tất cả thông tin của các đối tượng đó được sắp xết tăng dần theo tuổi và thêm một thuộc tính mới là shortName của mỗi đối tượng.*/
-var person = function (name, age, address) {
+var Person = function (name, age, address) {
   this.name = name;
   this.age = age;
   this.address = address;
 };
+function createCustomers(customerList) {
+  var newArray = customerList.map(function (customer) {
+    var firstName = customer.name.slice(0, customer.name.indexOf(" "));
+    var lastName = customer.name.slice(customer.name.lastIndexOf(" ") + 1);
+    var user = new Person(
+      `${customer.name}`,
+      customer.age,
+      `${customer.address}`
+    );
+    user.shortName = `${firstName} ${lastName}`;
+    return user;
+  });
+  newArray.sort(function (a, b) {
+    if (a.age < b.age) return -1;
+  });
+  return newArray;
+}
 
 const result = createCustomers(customers);
+console.log(result);
